b8d9823194242d8c88bd614fcdb618bf8d2322e7
Junaid Tahir <junaid@xevensolutions.com>
2023-02-10 07:29:19 +0500

    code-analyzer updated

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-#from flask import Flask
-#from flask import render_template
-
-#app = Flask(__name__)
-
-
-#@app.route("/")
-#def hello_world():
- #   return render_template("index.html")
-
-
 from flask import Flask, request, render_template, request
 import cv2
 import pytesseract
